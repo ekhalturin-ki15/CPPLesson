310ec3e3454d5574ea2959b273361717a7e43707
zaharuus22 <berlovzahar@gmail.com>
2025-12-19 19:04:52 +0700

    1.02

--- 800.cpp
+++ 800.cpp
@@ -12,7 +12,8 @@ int main()
 
 	int n;
 	cin >> n;
-
+	int m;
+	cin >> m;
 
 	int a[10000];
 
@@ -23,7 +24,7 @@ int main()
 			// 5
 			// 1 0 -1 0 3 -1 4 0 -1
 			//cout << i + 1 << " ";
-			a[i] = i/2 + 1;
+			a[i] = i / 2 + 1;
 			// x = 3 y = 2
 			// x = 5 y = 3
 			// x = 7 y = 4
@@ -42,23 +43,61 @@ int main()
 		{
 			if (i % 3 == 2)
 				a[i] = -1;
-		
+
 			++i;
 		}
 	}
+	int i = 0;
+	int g = 0;
+	while (i < (n * 2 - 1)) {
+		if (a[i] == 0) {
+			if (g < m) {
+				a[i] = -2;
+				g++;
+			}
+
+			else {
+				break;
+			}
+		}
+
+
+		i++;
+	}
+
+
 
 	{
 		int i = 0;
 		while (i < (n * 2 - 1))
 		{
-			cout << a[i] << " ";
+			cout << a[i] << "\t";
 
 			++i;
 		}
 	}
+	cout << "\n";
 
 
+	int b[100000];
+	{
+		int i = 0;
+		while (i < (n * 2 - 1)) {
+			b[i] = a[i] + a[i + 1];
 
+		
+			i++;
+		}
+		{
+			int i = 0;
+			while (i < (n * 2 - 1))
+			{
+				cout << b[i] << "\t";
+
+				++i;
+			}
+		}
+	}
 
 
 
