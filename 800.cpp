6bb93aaebd3fcc6c144f68b07fda14569d5bfd1e
Khalturin Evgenii <1tigrin@mail.ru>
2025-12-24 10:56:42 +0300

    v 0.1

--- 800.cpp
+++ 800.cpp
@@ -9,97 +9,59 @@ int main()
 	std::ifstream cin("input.txt");
 	std::ofstream cout("output.txt");
 
+	int i = 2;
+	int a = 5;
+	int b = 23;
 
-	int n;
-	cin >> n;
-	int m;
-	cin >> m;
+	// 16 гб
+	// 4 000 000 000
 
-	int a[10000];
+	long long R;
 
+	int h = 10;
+	int y = 100;
+
+	int u = i >> h;
+	//cout << b - a*(b/a);
+
+	//   b % a
+	//   b - (b/a) * a
+
+	// b = Q * a + R
+	// R = b - Q * a
+
+	//double
+	//float 
+
+
+	a = 5;
+	b = 21;
+	//int i = 2;
+	//int a = 5;
+	//int b = 25;
+
+	b = 1;
+	while (b < 100)
 	{
-		int i = 0;
-		while (i < (n * 2 - 1))
+		a = 1;
+		while (a < 100)
 		{
-			// 5
-			// 1 0 -1 0 3 -1 4 0 -1
-			//cout << i + 1 << " ";
-			a[i] = i / 2 + 1;
-			// x = 3 y = 2
-			// x = 5 y = 3
-			// x = 7 y = 4
-			// x = 9 y = 5
-			//y = 
-			a[i + 1] = 0;
-
-			i += 2;
+			cout << b << " " << a << " ";
+			cout << (b / a) << " ";
+			cout << ((b + a - 1) / a);
+			cout << "\n";
+			++a;
 		}
+
+		++b;
 	}
 
-
-	{
-		int i = 0;
-		while (i < (n * 2 - 1))
-		{
-			if (i % 3 == 2)
-				a[i] = -1;
-
-			++i;
-		}
-	}
-	int i = 0;
-	int g = 0;
-	while (i < (n * 2 - 1)) {
-		if (a[i] == 0) {
-			if (g < m) {
-				a[i] = -2;
-				g++;
-			}
-
-			else {
-				break;
-			}
-		}
-
-
-		i++;
-	}
-
-
-
-	{
-		int i = 0;
-		while (i < (n * 2 - 1))
-		{
-			cout << a[i] << "\t";
-
-			++i;
-		}
-	}
-	cout << "\n";
-
-
-	int b[100000];
-	{
-		int i = 0;
-		while (i < (n * 2 - 1)) {
-			b[i] = a[i] + a[i + 1];
-
-		
-			i++;
-		}
-		{
-			int i = 0;
-			while (i < (n * 2 - 1))
-			{
-				cout << b[i] << "\t";
-
-				++i;
-			}
-		}
-	}
-
-
+	
+	cout << ((a-1) / a) << " ";
+	cout << ((b + a - 1) / a);
+	//b + a - 1/ a
+	// b/a  + (a-1)/a
+	
 
 
 }
